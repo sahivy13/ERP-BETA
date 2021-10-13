cd08191ab9803c7859d130e355eaedba1514c040
sahivy13 <sahivy1990@gmail.com>
2021-10-13 14:37:11 -0400

    Started project and apps

---
+++ erp/itemhub/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ItemhubConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'itemhub'
---
+++ erp/rfqhub/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RfqhubConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'rfqhub'
